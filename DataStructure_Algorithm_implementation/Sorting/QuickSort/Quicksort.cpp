22ce4770f871e47482a6eb4b65157c1c99feed81
HaeUlNam <harrynam3@naver.com>
2019-02-20 16:23:59 +0900

    QuickSort

---
+++ DataStructure_Algorithm_implementation/Sorting/QuickSort/Quicksort.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+int arr[1000005];
+int N;
+
+void swap(int * a, int * b)
+{
+    int tmp = * a;
+    *a = *b;
+    *b = tmp;
+}
+
+int Partition(int start, int end)
+{
+    int pivotidx = start;
+    int pivot = arr[start];
+    int i = start;
+    int j = start + 1;
+
+    for(;j <= end;j++)
+    {
+        if(arr[j] < pivot)
+        {
+            i = i + 1;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    //pivot을 자신의 위치로 옮긴다.
+    swap(arr[pivotidx], arr[i]);
+
+    return i;
+}
+
+void QuickSort(int start, int end)
+{
+    if(start < end)
+    {
+        int mid = Partition(start, end);
+
+        QuickSort(start, mid - 1);
+        QuickSort(mid + 1, end);
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+
+    for(int i = 1; i <= N; i++)
+        cin >> arr[i];
+
+    QuickSort(1, N);
+
+    for(int i = 1; i <= N; i++)
+        cout << arr[i] << '\n';
+}
