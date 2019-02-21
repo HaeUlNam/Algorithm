bc608d388adab95c9fa52578896fbc54b30b457b
HaeUlNam <harrynam3@naver.com>
2019-02-21 22:00:04 +0900

    insertion sort

--- DataStructure_Algorithm_implementation/Sorting/InsertionSort/insertionsort.cpp
+++ DataStructure_Algorithm_implementation/Sorting/InsertionSort/insertionsort.cpp
@@ -1,14 +1,35 @@
 #include<iostream>
+using namespace std;
 
+int N;
+int arr[1005];
 
 void InsertionSort()
 {
-
+    for(int i = 1; i < N; i++)
+    {   
+        int tmp = arr[i];
+        int j;
+        for(j = i - 1; j >= 0; j--)
+        {
+            if(tmp >= arr[j]) break;
+            arr[j + 1] = arr[j];
+        }
+        arr[j + 1] = tmp;
+    }
 }
+
 int main()
 {
+    cin >> N;
+    
+    for(int i = 0; i < N; i++)
+        cin >> arr[i];
 
+    InsertionSort();
 
+    for(int i = 0; i < N; i++)
+        cout << arr[i] << endl;
 
     return 0;
 }
